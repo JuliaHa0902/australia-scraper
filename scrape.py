402ae5a5e1c653892bc4d61a1dec789d215e2033
JuliaHa0902 <43044560+JuliaHa0902@users.noreply.github.com>
2020-05-15 13:22:37 -0500

    Specify the website and css class

--- scrape.py
+++ scrape.py
@@ -10,8 +10,11 @@ from selenium.common.exceptions import TimeoutException
 class Scraper:
     """Class to scrape CNN latest news """
 
-    def __init__(self):
+    def __init__(self, website, load_css_class, headline_css_class):
         self.headline = set()
+        self.website = website
+        self.load_css_class = load_css_class
+        self.headline_css_class = headline_css_class
 
     def scraper(self):
         """Scraping CNN Australia latest news"""
@@ -20,14 +23,14 @@ class Scraper:
 
         browser = webdriver.Chrome(executable_path='C:/webdrivers/chromedriver', options=option)
 
-        browser.get("https://www.cnn.com/australia")
+        browser.get(self.website)
 
         #Wait 20 seconds for page to load
         time_out = 20
 
         try:
             #wait until the photo appear
-            WebDriverWait(browser, time_out).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='media__image media__image--responsive']")))
+            WebDriverWait(browser, time_out).until(EC.visibility_of_element_located((By.XPATH, self.load_css_class)))
 
         except TimeoutException:
             print("Time out for page to load")
@@ -35,7 +38,7 @@ class Scraper:
             return
 
         #find the latest news by find_elements_by_xpath
-        headline_element = browser.find_elements_by_xpath("//h3[@data-analytics='Australia latest_list-xs_article_']")
+        headline_element = browser.find_elements_by_xpath(self.headline_css_class)
         self.headline = {x.text for x in headline_element}
 
     def print_headline(self):
@@ -48,6 +51,6 @@ class Scraper:
                 print(title)
 
 if __name__ == "__main__":
-    cnn_headline = Scraper()
+    cnn_headline = Scraper("https://www.cnn.com/australia", "//img[@class='media__image media__image--responsive']", "//h3[@data-analytics='Australia latest_list-xs_article_']")
     cnn_headline.scraper()
     cnn_headline.print_headline()
